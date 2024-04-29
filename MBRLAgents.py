19f175dfc3c2e2e211e80e139422f3c079012c25
Ryan Ramdhan <ryanramdhan04@gmail.com>
2024-04-29 17:19:26 +0200

    initial commit

---
+++ MBRLAgents.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Model-based Reinforcement Learning policies
+Practical for course 'Reinforcement Learning',
+Bachelor AI, Leiden University, The Netherlands
+By Thomas Moerland
+"""
+import numpy as np
+from queue import PriorityQueue
+from MBRLEnvironment import WindyGridworld
+
+class DynaAgent:
+
+    def __init__(self, n_states, n_actions, learning_rate, gamma):
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.Q_sa = np.zeros((n_states,n_actions))
+        self.means_Q_sa = np.zeros((n_states, n_actions))
+        self.transition_counts = np.zeros((n_states,n_actions,n_states))
+        self.reward_sums = np.zeros((n_states,n_actions, n_states))
+        # TO DO: Initialize count tables, and reward sum tables. 
+        
+    def select_action(self, s, epsilon):
+        # TO DO: Change this to e-greedy action selection
+        if np.random.rand() < epsilon:
+            action = np.random.randint(0, self.n_actions)
+        else:
+            action = np.argmax(self.Q_sa[s])
+        
+        return action
+            
+        
+        
+    def update(self,s,a,r,done,s_next,n_planning_updates):
+        # TO DO: Add Dyna update
+        
+        self.transition_counts[s,a,s_next] += 1
+        self.reward_sums[s,a,s_next] += r
+        estimate_transition = self.transition_counts[s,a,s_next] / np.sum(self.transition_counts[s,a, :])
+        estimate_r = self.reward_sums[s,a,s_next] / self.transition_counts[s,a,s_next]
+        self.Q_sa[s,a] += self.learning_rate * (r + self.gamma * np.max(self.Q_sa[s_next]) - self.Q_sa[s,a])
+        
+        for k in range(n_planning_updates):
+            s = np.random.choice(np.where(self.transition_counts[s,a] > 0)[0])
+            a = np.random.choice(np.where(self.transition_counts[s] > 0)[0])
+            s_next = np.random.choice(np.where(estimate_transition * self.transition_counts[s,a] > 0)[0])
+            self.Q_sa[s,a] += self.learning_rate * (estimate_r + self.gamma * np.max(self.Q_sa[s_next]) - self.Q_sa[s,a])
+
+    def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
+        returns = []  # list to store the reward per episode
+        for i in range(n_eval_episodes):
+            s = eval_env.reset()
+            R_ep = 0
+            for t in range(max_episode_length):
+                a = np.argmax(self.Q_sa[s]) # greedy action selection
+                s_prime, r, done = eval_env.step(a)
+                R_ep += r
+                if done:
+                    break
+                else:
+                    s = s_prime
+            returns.append(R_ep)
+        mean_return = np.mean(returns)
+        return mean_return
+
+class PrioritizedSweepingAgent:
+
+    def __init__(self, n_states, n_actions, learning_rate, gamma, priority_cutoff=0.01):
+        self.n_states = n_states
+        self.n_actions = n_actions
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.priority_cutoff = priority_cutoff
+        self.queue = PriorityQueue()
+        
+        self.Q_sa = np.zeros((n_states,n_actions))
+        # TO DO: Initialize count tables, reward sum tables, priority queue
+        
+    def select_action(self, s, epsilon):
+        # TO DO: Change this to e-greedy action selection
+        a = np.random.randint(0,self.n_actions) # Replace this with correct action selection
+        return a
+        
+    def update(self,s,a,r,done,s_next,n_planning_updates):
+        
+        # TO DO: Add Prioritized Sweeping code
+        
+        # Helper code to work with the queue
+        # Put (s,a) on the queue with priority p (needs a minus since the queue pops the smallest priority first)
+        # self.queue.put((-p,(s,a))) 
+        # Retrieve the top (s,a) from the queue
+        # _,(s,a) = self.queue.get() # get the top (s,a) for the queue
+        pass
+
+    def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
+        returns = []  # list to store the reward per episode
+        for i in range(n_eval_episodes):
+            s = eval_env.reset()
+            R_ep = 0
+            for t in range(max_episode_length):
+                a = np.argmax(self.Q_sa[s]) # greedy action selection
+                s_prime, r, done = eval_env.step(a)
+                R_ep += r
+                if done:
+                    break
+                else:
+                    s = s_prime
+            returns.append(R_ep)
+        mean_return = np.mean(returns)
+        return mean_return        
+
+def test():
+
+    n_timesteps = 10001
+    gamma = 1.0
+
+    # Algorithm parameters
+    policy = 'dyna' # or 'ps' 
+    epsilon = 0.1
+    learning_rate = 0.2
+    n_planning_updates = 3
+
+    # Plotting parameters
+    plot = True
+    plot_optimal_policy = True
+    step_pause = 0.0001
+    
+    # Initialize environment and policy
+    env = WindyGridworld()
+    if policy == 'dyna':
+        pi = DynaAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize Dyna policy
+    elif policy == 'ps':    
+        pi = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize PS policy
+    else:
+        raise KeyError('Policy {} not implemented'.format(policy))
+    
+    # Prepare for running
+    s = env.reset()  
+    continuous_mode = False
+    
+    for t in range(n_timesteps):            
+        # Select action, transition, update policy
+        a = pi.select_action(s,epsilon)
+        s_next,r,done = env.step(a)
+        pi.update(s=s,a=a,r=r,done=done,s_next=s_next,n_planning_updates=n_planning_updates)
+        
+        # Render environment
+        if plot:
+            env.render(Q_sa=pi.Q_sa,plot_optimal_policy=plot_optimal_policy,
+                       step_pause=step_pause)
+            
+        # Ask user for manual or continuous execution
+        if not continuous_mode:
+            key_input = input("Press 'Enter' to execute next step, press 'c' to run full algorithm")
+            continuous_mode = True if key_input == 'c' else False
+
+        # Reset environment when terminated
+        if done:
+            s = env.reset()
+        else:
+            s = s_next
+            
+    
+if __name__ == '__main__':
+    test()
---
+++ MBRLExperiment.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Model-based Reinforcement Learning experiments
+Practical for course 'Reinforcement Learning',
+Bachelor AI, Leiden University, The Netherlands
+By Thomas Moerland
+"""
+import numpy as np
+from MBRLEnvironment import WindyGridworld
+from MBRLAgents import DynaAgent, PrioritizedSweepingAgent
+from Helper import LearningCurvePlot, smooth
+
+def experiment():
+    n_timesteps = 10001
+    eval_interval = 250
+    n_repetitions = 10
+    gamma = 1.0
+    learning_rate = 0.2
+    epsilon=0.1
+    
+    wind_proportions=[0.9,1.0]
+    n_planning_updatess = [1,3,5] 
+    
+    # IMPLEMENT YOUR EXPERIMENT HERE
+
+def run_repetitions(n_timesteps=1000, n_repetitions=10, gamma=1.0, learning_rate=0.2, epsilon=0.1, wind_proportion=0.9, n_planning_updates=5):
+    # IMPLEMENT YOUR FUNCTION HERE
+    # This function should run n_repetitions of the Dyna-Q algorithm on the WindyGridworld environment
+    # It should return the average return per timestep over all repetitions
+    # (This is the value that you should plot in the end)
+    env = WindyGridworld(wind_proportion=wind_proportion)
+    agent = DynaAgent(env.n_states, env.n_actions, learning_rate, gamma)
+    mean_rewards = np.zeros((n_timesteps))
+    for repetition in range(n_repetitions):
+        s = env.reset()
+        for timestep in range(n_timesteps):
+            a = agent.select_action(s, epsilon)
+            s_next, r, done = env.step(a)
+            agent.update(s, a, r, done, s_next, n_planning_updates)
+            mean_rewards[timestep] += r
+            if done:
+                break
+            s = s_next
+    mean_rewards /= n_repetitions
+    print(mean_rewards)
+    
+            
+    
+    
+    
+    
+if __name__ == '__main__':
+    experiment()
